19e62be57e50c4d8b20fa64e6d52708ee25035df
Georgi Pastrakov <160168282+GeorgiMPastrakov@users.noreply.github.com>
2025-05-05 19:19:24 +0300

    Add task interface implementation

---
+++ homework/5.5.2025/task_interface.c
@@ -0,0 +1,18 @@
+#include  <stdio.h>
+#include  <stdlib.h>
+#include "lessons/17-dynamic-array/9v/DynamicArray.h"
+#include "lessons/17-dynamic-array/9v/util.h"
+#include "task_interface.h"
+
+int findIndex(DynamicArray *arr, DynArrType element){
+    if (arr->size == 0){
+        printf("No elements in the array.");
+        return -1;
+    }
+    for(int i = 0; i < arr->size; i++){
+        if(arr->buffer[i] == element){
+            return i;
+        }
+    }
+return -1;
+}
---
+++ homework/5.5.2025/task_interface.h
@@ -0,0 +1,10 @@
+#ifndef TASKINTERFACE
+#define TASKINTERFACE
+#include  <stdio.h>
+#include  <stdlib.h>
+#include "lessons/17-dynamic-array/9v/DynamicArray.h"
+#include "lessons/17-dynamic-array/9v/util.h"
+
+int findIndex(DynamicArray *arr, DynArrType element);
+
+#endif
